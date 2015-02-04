95ad61457642922a43e05abf76bc87d00ca620d7
sbatzka <Steve@Stevens-MacBook-Pro.local>
2015-02-04 02:39:05 -0600

    Finished Create and Delete

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,4 +7,22 @@ def show
   @photo = Photo.find_by({ :id => params[:id] })
 end
 
+def new_form
+
+end
+
+def create_row
+p = Photo.new
+p.caption = params[:the_caption]
+p.source = params[:the_source]
+p.save
+
+end
+
+def destroy
+p = Photo.find_by( :id => params[:id])
+p.destroy
+
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
 
+# Routes to create new photo
+get("/photos/new", { :controller => "photos", :action => "new_form" })
+get("/create_photo", { :controller => "photos", :action => "create_row" })
+
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
 
+# Route to delete a photo
+get("/delete_photo/:id", { :controller => "photos", :action => "destroy" })
 end
